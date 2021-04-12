8a080a8676bf08e4bc0f95a31ce72364b42e3f86
monicamarshall <61441459+monicamarshall@users.noreply.github.com>
2021-04-12 14:40:27 -0600

    Add files via upload
    initial commit

---
+++ bulkload/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BulkloadConfig(AppConfig):
+    name = 'bulkload'
---
+++ crudgenerics/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrudgenericsConfig(AppConfig):
+    name = 'crudgenerics'
